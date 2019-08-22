87d8b07f83d4d29ead742dc61d96ab999bf6ee38
Ian <mus5002@aol.com>
2019-08-21 21:24:58 -0400

    added function for countdown timer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0)
   const [awayScore, setAwayScore] = useState(0)
+  const [time, setTime] = useState('15:00')
+
+  const handleChange = (team, score) => {
+    if(team === 'home') return setHomeScore(homeScore+score)
+    else return setAwayScore(awayScore+score)
+  }
+
+  const startGame = () => {
+    let currentTime = 1500
+    let timer = setInterval(() => {
+      currentTime--
+      let newTime = currentTime.toString().slice(0,2) + ':' + currentTime.toString().slice(2,4)
+      setTime(newTime)
+      console.log('counting : ', currentTime)
+      if(currentTime === 0) {
+        clearInterval(timer)
+      }
+    }, 1000)
+  }
 
   return (
     <div className="container">
@@ -19,7 +38,7 @@ function App() {
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{time}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
@@ -28,14 +47,15 @@ function App() {
         <BottomRow />
       </section>
       <section className="buttons">
+        <button onClick={() => startGame()}>Start Game</button>
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore+7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore+3)}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => handleChange('home', 7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => handleChange('home', 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore+7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awayScore+3)}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => handleChange('away', 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => handleChange('away', 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
